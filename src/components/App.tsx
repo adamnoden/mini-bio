9031020b0cc16d1a5e7ada93d1731442d0dd7f52
Adam <107278274+adamnoden@users.noreply.github.com>
2025-01-09 21:41:16 +0000

    paper

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import profile from "../assets/profile.png";
 import tate from "../assets/tate.png";
+import paper from "../assets/paper.png";
 import { QRCodeSVG } from "qrcode.react";
 
 function App() {
@@ -30,104 +31,114 @@ function App() {
           }}
           alt="profile"
         />
-
         <div
           style={{
-            width: 235,
-            height: 208,
             background: "#faf9f0",
-            borderRight: "1px dashed #ddd",
-            padding: "20px",
-            justifyContent: "space-between",
-            fontFamily: "'Arial', sans-serif",
-            color: "#313302",
-            position: "relative",
+            // background: "red",
+            backgroundImage: `url(${paper})`, // Path to your texture
+            backgroundSize: "cover", // Ensures the texture covers the entire div
+            backgroundRepeat: "no-repeat", // Prevents tiling
+            backgroundPosition: "center", // Centers the texture
+            display: "flex",
           }}
         >
           <div
             style={{
-              fontWeight: "bold",
-              fontSize: 24,
-              marginBottom: 15,
-            }}
-            className="montserrat-extrabold"
-          >
-            ANALEOVY
-          </div>
-          <div
-            style={{
-              fontSize: 14,
-              marginBottom: 15,
-            }}
-          >
-            Adult Ticket
-          </div>
-          <div
-            style={{
-              fontSize: 14,
-              marginBottom: 30,
+              width: 235,
+              height: 208,
+              borderRight: "1px dashed #ddd",
+              padding: "20px",
+              justifyContent: "space-between",
+              fontFamily: "'Arial', sans-serif",
+              color: "#313302",
+              position: "relative",
             }}
           >
             <div
               style={{
                 fontWeight: "bold",
+                fontSize: 24,
+                marginBottom: 15,
+              }}
+              className="montserrat-extrabold"
+            >
+              ANALEOVY
+            </div>
+            <div
+              style={{
+                fontSize: 14,
+                marginBottom: 15,
               }}
             >
-              20.11.2020
+              Adult Ticket
             </div>
-            <div>Saturday 11am - 6pm</div>
+            <div
+              style={{
+                fontSize: 14,
+                marginBottom: 30,
+              }}
+            >
+              <div
+                style={{
+                  fontWeight: "bold",
+                }}
+              >
+                20.11.2020
+              </div>
+              <div>Saturday 11am - 6pm</div>
+            </div>
+            <div
+              style={{
+                fontSize: 11,
+                lineHeight: "1.5",
+              }}
+            >
+              Tate Britain
+              <br />
+              Milbank Str. 13B
+              <br />
+              London, UK
+            </div>
+            <img
+              src={tate}
+              style={{
+                width: 55,
+                position: "absolute",
+                right: 24,
+                bottom: 24,
+              }}
+              alt="tate"
+            />
           </div>
           <div
             style={{
-              fontSize: 11,
-              lineHeight: "1.5",
+              width: 62,
+              height: 208,
+              color: "#313302",
+              padding: 20,
             }}
           >
-            Tate Britain
-            <br />
-            Milbank Str. 13B
-            <br />
-            London, UK
-          </div>
-          <img
-            src={tate}
-            style={{
-              width: 55,
-              position: "absolute",
-              right: 24,
-              bottom: 24,
-            }}
-            alt="tate"
-          />
-        </div>
-        <div
-          style={{
-            width: 62,
-            height: 208,
-            background: "#faf9f0",
-            color: "#313302",
-            padding: 20,
-          }}
-        >
-          <div
-            style={{
-              fontSize: 11,
-              lineHeight: "1.5",
-              marginBottom: 98,
-            }}
-          >
-            1 day pass
-            <br />
-            20.11.2020
-            <br />
-            K3-8693-11
-          </div>
+            <div
+              style={{
+                fontSize: 11,
+                lineHeight: "1.5",
+                marginBottom: 98,
+              }}
+            >
+              1 day pass
+              <br />
+              20.11.2020
+              <br />
+              K3-8693-11
+            </div>
 
-          <QRCodeSVG
-            color={"#525409"}
-            size={60}
-            value="https://www.lockheedmartin.com/"
-          />
+            <QRCodeSVG
+              fgColor="#525409"
+              bgColor="#faf9f0"
+              size={56}
+              value="https://www.lockheedmartin.com/"
+            />
+          </div>
         </div>
       </div>
     </div>
